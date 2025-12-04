40e492fba78c1060179fc291f115a61357f733bd
David Moreno <davidbreakeam5@gmail.com>
2025-12-03 22:08:25 -0600

    xml crear cuentas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation ('com.google.android.material:material:1.6.1')
 }
